a51003133a69d37387219025ed169c49a4909dbb
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-01-27 17:53:08 -0500

    refactored HTML and restarted JS

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,2 @@
-//https://api.openweathermap.org / data / 2.5 / forecast ? lat = { lat } & lon={ lon }& appid=weather
-
-var cities = [];
 
 
-$("#search-city").on('click', function (event) {
-    event.preventDefault();
-    var city = $('#city-input').val().trim()
-
-    cities.push(city);
-
-    // displayCity();
-    // searchHistory();
-
-
-});
-
-//figure out api
-
-// create function that pulls 5 day forcast from api
-
-//create a current weather function for main card
-
-// store searched values and display in history
